f34886685a7d3029954eb2aa1360c56a54ec4732
moonRider <solar.pom@gmail.com>
2017-02-16 15:32:51 +0800

    add simple server to project

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 const gulp = require('gulp');
+const server = require('./index').server;
 
 gulp.task('default', function() {
+  server.start();
 });
---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('dist'));
+
+app.start = () => {
+  app.listen(4000, ()=>{ console.log('Example app listening on port 4000!'); });
+};
+
+exports.server = app;

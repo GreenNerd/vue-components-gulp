1b825d4d45ae0b377fe603d017f4cb7e6dc63f27
moonRider <solar.pom@gmail.com>
2017-02-16 15:48:24 +0800

    add simple sass compile task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const server = require('./index').server;
+const sass = require('gulp-sass');
 
-gulp.task('default', function() {
+gulp.task('sass', ()=>{
+  gulp.src('src/**/*.scss')
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['sass'], function() {
   server.start();
 });

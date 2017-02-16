160e363042527768f97865a2c88fdbcb13657177
moonRider <solar.pom@gmail.com>
2017-02-16 15:59:50 +0800

    add coffee compile task to project

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 const gulp = require('gulp');
 const server = require('./index').server;
 const sass = require('gulp-sass');
+const coffee = require('gulp-coffee');
+
+
+gulp.task('coffee', function() {
+  gulp.src('src/**/*.coffee')
+    .pipe(coffee({ bare: false }))
+    .pipe(gulp.dest('dist'));
+});
 
 gulp.task('sass', ()=>{
   gulp.src('src/**/*.scss')
